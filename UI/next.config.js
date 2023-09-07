12c0bd548d3349eb8a7129a21dc43fc0f743462a
JohnDennisJackson <JohnDennisJackson@users.noreply.github.com>
2023-09-06 23:52:48 -0700

    init commit

---
+++ UI/next.config.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+    const rewrites = () => {
+      return [
+        {
+          source: "/hello/:path*",
+          destination: "http://127.0.0.1:5000/api/hello/:path*",
+        },
+      ];
+    };
+    return {
+      rewrites,
+    };
+  };
